19b1e1e2ecf2fb098e07c43d8a36b8a1824e0101
SOFIA <rusofiaru@gmail.com>
2020-10-14 16:31:14 +0300

    creating users + tests

--- tests/test_helper.js
+++ tests/test_helper.js
@@ -1,6 +1,7 @@
 const blogs = require('./data')
 
 const Blog = require('../models/blog')
+const User = require('../models/user')
 
 const initialBlogs = blogs.slice()
 
@@ -21,6 +22,12 @@ const blogById = async (id) => {
     const blog = await Blog.findById(id)
     return blog.toJSON()
 }
+
+const usersInDb = async () => {
+    const users = await User.find({})
+    return users.map(u => u.toJSON())
+}
+
 module.exports = {
-    initialBlogs, nonExistingId, blogsInDb, blogById
+    initialBlogs, nonExistingId, blogsInDb, blogById, usersInDb
 }
